7b14b733b9483ed478b15b8de3336ce7c62865e0
DuyBao1001 <24520190@gm.uit.edu.vn>
2025-12-12 10:37:01 +0700

    done bai1

--- Bai1/Form1.Designer.cs
+++ Bai1/Form1.Designer.cs
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxFrom = new System.Windows.Forms.TextBox();
+            this.txtBoxTo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxSubject = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rtBoxBody = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(49, 62);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(124, 64);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "From:";
+            // 
+            // txtBoxFrom
+            // 
+            this.txtBoxFrom.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxFrom.Location = new System.Drawing.Point(340, 46);
+            this.txtBoxFrom.Name = "txtBoxFrom";
+            this.txtBoxFrom.Size = new System.Drawing.Size(415, 31);
+            this.txtBoxFrom.TabIndex = 2;
+            // 
+            // txtBoxTo
+            // 
+            this.txtBoxTo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxTo.Location = new System.Drawing.Point(340, 105);
+            this.txtBoxTo.Name = "txtBoxTo";
+            this.txtBoxTo.Size = new System.Drawing.Size(415, 31);
+            this.txtBoxTo.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "To:";
+            // 
+            // txtBoxSubject
+            // 
+            this.txtBoxSubject.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxSubject.Location = new System.Drawing.Point(182, 177);
+            this.txtBoxSubject.Name = "txtBoxSubject";
+            this.txtBoxSubject.Size = new System.Drawing.Size(962, 31);
+            this.txtBoxSubject.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Subject:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Body:";
+            // 
+            // rtBoxBody
+            // 
+            this.rtBoxBody.Location = new System.Drawing.Point(167, 252);
+            this.rtBoxBody.Name = "rtBoxBody";
+            this.rtBoxBody.Size = new System.Drawing.Size(977, 386);
+            this.rtBoxBody.TabIndex = 8;
+            this.rtBoxBody.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1238, 665);
+            this.Controls.Add(this.rtBoxBody);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBoxSubject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxFrom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Bài 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxFrom;
+        private System.Windows.Forms.TextBox txtBoxTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxSubject;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rtBoxBody;
     }
 }
 
--- Bai1/Form1.cs
+++ Bai1/Form1.cs
@@ -1,20 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace Bai1
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string from = txtBoxFrom.Text.Trim();
+                string to = txtBoxTo.Text.Trim();
+                string subject = txtBoxSubject.Text;
+                string body = rtBoxBody.Text;
+
+                if (string.IsNullOrWhiteSpace(from) || string.IsNullOrWhiteSpace(to) || string.IsNullOrWhiteSpace(subject) || string.IsNullOrWhiteSpace(body))
+                {
+                    MessageBox.Show("Vui lòng điền đầy đủ thông tin.");
+                    return;
+                }
+
+                string password = "risa qxfc ngun rvsd";
+
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(from, from));
+                message.To.Add(new MailboxAddress("", to));
+                message.Subject = subject;
+                message.Body = new TextPart("plain")
+                {
+                    Text = body
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 465, true);
+                    client.Authenticate(from, password);
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+
+                MessageBox.Show("Email đã được gửi thành công!", "Thông báo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Đã xảy ra lỗi khi gửi email: {ex.Message}", "Lỗi");
+            }
         }
     }
 }
