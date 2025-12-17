2023ab1877e86400d7c858ca3a2dfe28e990baa6
DuyBao1001 <24520190@gm.uit.edu.vn>
2025-12-17 20:01:56 +0700

    done Bai4

---
+++ Bai4/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bai4
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string DetailUrl { get; set; }
+        public int Price { get; set; } = 40000;
+    }
+    public class OrderInfo
+    {
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public string MovieName { get; set; }
+        public List<string> Seats { get; set; }
+        public long TotalPrice { get; set; }
+        public DateTime BookingTime { get; set; }
+    }
+}
